03b6c8d6b7518689137e0260b89b317e96dccd6b
Omar Ferrer <Omar Ferrer>
2016-09-27 00:53:51 -0400

    Added a script to allow setting up sqlite directory

---
+++ cli-files/scripts/setup-windows-env.ps1
@@ -0,0 +1,3 @@
+# Download from sqlite-dll-win64 from https://sqlite.org/download.html
+# $env:SQLITE3_LIB_DIR="C:\Users\omarf\Downloads\sqlite-dll-win64-x64-3140200"
+$env:SQLITE3_LIB_DIR=".\NEVER_COMMIT\sqlite_windows"
