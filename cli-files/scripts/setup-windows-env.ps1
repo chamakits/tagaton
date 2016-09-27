890bb5d6d7b456e366e0f3dc9f1344ab6cab1d5f
Omar Ferrer <Omar Ferrer>
2016-09-27 01:11:15 -0400

    Commented out the openssL

--- cli-files/scripts/setup-windows-env.ps1
+++ cli-files/scripts/setup-windows-env.ps1
@@ -1,3 +1,7 @@
 # Download from sqlite-dll-win64 from https://sqlite.org/download.html
 # $env:SQLITE3_LIB_DIR="C:\Users\omarf\Downloads\sqlite-dll-win64-x64-3140200"
 $env:SQLITE3_LIB_DIR=".\NEVER_COMMIT\sqlite_windows"
+# $env:OPENSSL_LIB_DIR=".\NEVER_COMMIT\openssl_windows"
+$env:OPENSSL_LIB_DIR=".\NEVER_COMMIT\openssl_windows64"
+$env:OPENSSL_INCLUDE_DIR=".\NEVER_COMMIT\openssl_windows64\include"
+# $env:OPENSSL_INCLUDE_DIR=".\NEVER_COMMIT\openssl_windows\include"
