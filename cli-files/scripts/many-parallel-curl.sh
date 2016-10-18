ad023eaecbdf8966f425b61f6111ec770bbf6287
Omar Ferrer <omar.ferrer.uprm@gmail.com>
2016-10-17 22:40:48 -0400

    Made script purposely slow down a bit

--- cli-files/scripts/many-parallel-curl.sh
+++ cli-files/scripts/many-parallel-curl.sh
@@ -3,5 +3,14 @@
 repeat_amount="$1"
 url="$2"
 
-seq "$repeat_amount" | parallel -j0 --joblog log curl -s "$url"
+function sleep_for_time_then_run {
+    number_in="$1"
+    url="$2"
+    time_sleep=$(bc -l <<< "$number_in / 100")
+    sleep "$time_sleep"
+    curl -s "$url"
+}
+export -f sleep_for_time_then_run
+
+seq "$repeat_amount" | parallel -j0 --joblog log sleep_for_time_then_run {} $url
 cut -f 4 log
